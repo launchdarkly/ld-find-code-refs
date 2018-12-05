5a45b900ba1e7612683379d2e230ab9074ee9d3f
Arnold Trakhtenberg <arnoldtrakh@gmail.com>
2018-12-05 13:53:00 -0800

    [ch27867] Exit early when github actions environment variables are improperly set

--- parse/github-actions/github-actions.go
+++ parse/github-actions/github-actions.go
@@ -6,24 +6,24 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 
 	"github.com/launchdarkly/git-flag-parser/parse"
+	"github.com/launchdarkly/git-flag-parser/parse/internal/log"
 	o "github.com/launchdarkly/git-flag-parser/parse/internal/options"
 )
 
 func main() {
-	fmt.Println("Setting GitHub action env vars")
+	log.Info("Setting GitHub action env vars", nil)
 	ghRepo := strings.Split(os.Getenv("GITHUB_REPOSITORY"), "/")
 	if len(ghRepo) < 2 {
-		fmt.Fprintf(os.Stderr, "Invalid GitHub repository name set: '%s'\n", os.Getenv("GITHUB_REPOSITORY"))
-		os.Exit(1)
+		log.Fatal("Invalid GitHub repository name", fmt.Errorf("unable to find GitHub repository name"))
 	}
 	event, err := parseEvent(os.Getenv("GITHUB_EVENT_PATH"))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error parsing GitHub event payload: %+v at '%s'\n", err, os.Getenv("GITHUB_EVENT_PATH"))
-		os.Exit(1)
+		log.Fatal("Error parsing GitHub event payload", fmt.Errorf("unable to parse GitHub event payload: %+v at '%s'\n", err, os.Getenv("GITHUB_EVENT_PATH")))
 	}
 
 	options := map[string]string{
@@ -40,15 +40,22 @@ func main() {
 		"pushTime":      strconv.FormatInt(event.Repo.PushedAt*1000, 10), // seconds to milliseconds
 		"defaultBranch": event.Repo.DefaultBranch,
 	}
+	ldOptions, err := getLDOptionsFromEnv()
+	if err != nil {
+		log.Fatal("Error settings options", err)
+	}
+	for k, v := range ldOptions {
+		options[k] = v
+	}
+
 	o.Populate()
 	for k, v := range options {
 		err := flag.Set(k, v)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error setting option: %s", k)
-			os.Exit(1)
+			log.Fatal(fmt.Sprintf("Error setting option %s", k), err)
 		}
 	}
-	fmt.Fprintf(os.Stderr, "Starting repo parsing program with options:\n %+v\n", options)
+	log.Info(fmt.Sprintf("Starting repo parsing program with options:\n %+v\n", options), nil)
 	parse.Parse()
 }
 
@@ -83,4 +90,28 @@ func parseEvent(path string) (*Event, error) {
 		return nil, err
 	}
 	return &evt, err
+}
+
+func getLDOptionsFromEnv() (map[string]string, error) {
+	ldOptions := map[string]string{
+		"accessToken":  os.Getenv("LD_ACCESS_TOKEN"),
+		"projKey":      os.Getenv("LD_PROJ_KEY"),
+		"exclude":      os.Getenv("LD_EXCLUDE"),
+		"contextLines": os.Getenv("LD_CONTEXT_LINES"),
+		"baseUri":      os.Getenv("LD_BASE_URI"),
+	}
+	_, err := regexp.Compile(ldOptions["exclude"])
+	if err != nil {
+		return ldOptions, fmt.Errorf("couldn't parse LD_EXCLUDE as regex: %+v", err)
+	}
+
+	if ldOptions["contextLines"] == "" {
+		ldOptions["contextLines"] = "-1"
+	}
+	_, err = strconv.ParseInt(ldOptions["contextLines"], 10, 32)
+	if err != nil {
+		return ldOptions, fmt.Errorf("coudln't parse LD_CONTEXT_LINES as an integer: %+v", err)
+	}
+
+	return ldOptions, nil
 }
