ab8a42b46b5612a2a5daa77e9754d86fcd60ed10
atrakh <arnoldtrakh@gmail.com>
2019-02-13 16:55:32 -0800

    address review

--- internal/command/command_test.go
+++ internal/command/command_test.go
@@ -17,19 +17,23 @@ func TestGenerateFlagRegex(t *testing.T) {
 		{
 			name:     "succeeds for single flag",
 			flags:    []string{"flag"},
-			expected: "flag"},
+			expected: "flag",
+		},
 		{
 			name:     "succeeds for single flag with escape char",
 			flags:    []string{"^flag"},
-			expected: `\^flag`},
+			expected: `\^flag`,
+		},
 		{
 			name:     "succeeds for multiple flags",
 			flags:    []string{"flag1", "flag2"},
-			expected: "flag1|flag2"},
+			expected: "flag1|flag2",
+		},
 		{
 			name:     "succeeds for multiple flags with escape characters",
 			flags:    []string{"^flag1", ".flag2", "*flag3"},
-			expected: `\^flag1|\.flag2|\*flag3`},
+			expected: `\^flag1|\.flag2|\*flag3`,
+		},
 	}
 	for _, tt := range specs {
 		t.Run(tt.name, func(t *testing.T) {
