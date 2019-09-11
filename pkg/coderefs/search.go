51799ef90c3f4ae9453fcd8806dc9a36a856b8bc
atrakh <arnold@launchdarkly.com>
2019-09-10 17:42:50 -0700

    add unit tests for paginated searches

--- pkg/coderefs/search.go
+++ pkg/coderefs/search.go
@@ -16,6 +16,11 @@ var (
 // simplePaginatedSearch attempts to search by reducing pageSize until a valid search is generated
 func simplePaginatedSearch(cmd command.Client, flags []string, pageSize, ctxLines int, delims []rune) ([][]string, error) {
 	var results [][]string
+	// base case: cannot search for 0 flags at a time
+	if pageSize <= 0 {
+		return nil, NoSearchPatternErr
+	}
+
 	for from := 0; from < len(flags); from += pageSize {
 		to := from + pageSize
 		if to > len(flags) {
@@ -28,10 +33,7 @@ func simplePaginatedSearch(cmd command.Client, flags []string, pageSize, ctxLine
 			if err == command.SearchTooLargeErr {
 				nextPageSize := pageSize / 2
 				log.Debug.Printf("encountered an error paginating at pagesize %d, attempting with a pagesize of %d", pageSize, nextPageSize)
-				if nextPageSize > 0 {
-					return simplePaginatedSearch(cmd, flags, pageSize, ctxLines, delims)
-				}
-				return nil, NoSearchPatternErr
+				return simplePaginatedSearch(cmd, flags, nextPageSize, ctxLines, delims)
 			}
 			return nil, err
 		}
@@ -40,13 +42,16 @@ func simplePaginatedSearch(cmd command.Client, flags []string, pageSize, ctxLine
 	return results, nil
 }
 
-// paginatedSearch attempts to come up with a maximal set of flag keys to search for at a time dependent on the value set by safePaginationCharCount
+// paginatedSearch uses approximations to decide the number of flags to scan for at once using maxSumFlagKeyLength as an upper bound
 func paginatedSearch(cmd command.Client, flags []string, maxSumFlagKeyLength, ctxLines int, delims []rune) ([][]string, error) {
 	var results [][]string
 	nextSearchKeys := []string{}
+
+	// The approximate length of delimeters when added to the beginning and end of a search pattern
 	totalKeyLength := len(delims) * 2
 	from := 0
 	for to, key := range flags {
+		// periods need to be escaped, so they count as 2 characters
 		totalKeyLength += len(key) + strings.Count(key, ".")
 		if totalKeyLength > maxSumFlagKeyLength {
 			log.Debug.Printf("searching for flags in group: [%d, %d]", from, to)
@@ -54,11 +59,11 @@ func paginatedSearch(cmd command.Client, flags []string, maxSumFlagKeyLength, ct
 			if err != nil {
 				if err == command.SearchTooLargeErr {
 					// this shouldn't be possible with a valid maxSumFlagKeyLength
-					if len(nextSearchKeys) <= 0 {
+					if len(nextSearchKeys) == 0 {
 						return nil, FatalPaginatedSearchErr
 					}
 
-					// we expect all search implementations to complete successfully,
+					// we expect all search implementations to complete successfully
 					// if pagination fails unexpectedly, fallback to a simple search algorithm for the remaining pages
 					log.Debug.Printf("encountered an error paginating, falling back to simple paginated search")
 					remainder, err := simplePaginatedSearch(cmd, flags[from:], len(nextSearchKeys)-1, ctxLines, delims)
@@ -70,7 +75,8 @@ func paginatedSearch(cmd command.Client, flags []string, maxSumFlagKeyLength, ct
 				return nil, err
 			}
 			results = append(results, result...)
-			nextSearchKeys = []string{}
+			// capacity of the next search is likely similar to the previous search
+			nextSearchKeys = make([]string, 0, len(nextSearchKeys))
 			totalKeyLength = len(delims) * 2
 			from = to + 1
 		}
---
+++ pkg/coderefs/search_test.go
@@ -0,0 +1,129 @@
+package coderefs
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/launchdarkly/ld-find-code-refs/internal/command"
+)
+
+type MockClient struct {
+	results [][]string
+	err     error
+}
+
+func (c MockClient) RemoteBranches() (map[string]bool, error) {
+	return nil, errors.New("Mock error")
+}
+
+func (c MockClient) SearchForFlags(flags []string, ctxLines int, delimiters []rune) ([][]string, error) {
+	return c.results, c.err
+}
+
+func Test_paginatedSearch(t *testing.T) {
+	specs := []struct {
+		name                string
+		maxSumFlagKeyLength int
+		mockResults         [][]string
+		mockErr             error
+		expectedResults     [][]string
+		expectedErr         error
+	}{
+		{
+			name:                "returns results with 1 page",
+			mockResults:         [][]string{{"hello"}},
+			expectedResults:     [][]string{{"hello"}},
+			maxSumFlagKeyLength: 20,
+		},
+		{
+			name:                "combines results with multiple pages",
+			mockResults:         [][]string{{"hello"}},
+			expectedResults:     [][]string{{"hello"}, {"hello"}},
+			maxSumFlagKeyLength: 5,
+		},
+		{
+			name:                "pagination fails when client fails to generate a search pattern",
+			mockErr:             command.SearchTooLargeErr,
+			expectedErr:         NoSearchPatternErr,
+			maxSumFlagKeyLength: 10,
+		},
+		{
+			// this case should be impossible outside of tests
+			name:                "pagination fails when maxSumFlagKeyLength is too low",
+			mockErr:             command.SearchTooLargeErr,
+			expectedErr:         FatalPaginatedSearchErr,
+			maxSumFlagKeyLength: 0,
+		},
+	}
+
+	for _, tt := range specs {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := paginatedSearch(
+				MockClient{
+					results: tt.mockResults,
+					err:     tt.mockErr,
+				},
+				[]string{"someFlag", "anotherFlag"},
+				tt.maxSumFlagKeyLength,
+				0,
+				[]rune{'"'},
+			)
+			assert.Equal(t, tt.expectedResults, res)
+			assert.Equal(t, tt.expectedErr, err)
+		})
+	}
+}
+
+func Test_simplePaginatedSearch(t *testing.T) {
+	specs := []struct {
+		name             string
+		startingPageSize int
+		mockResults      [][]string
+		mockErr          error
+		expectedResults  [][]string
+		expectedErr      error
+	}{
+		{
+			name:             "returns results with 1 page",
+			startingPageSize: 2,
+			mockResults:      [][]string{{"hello"}},
+			expectedResults:  [][]string{{"hello"}},
+		},
+		{
+			name:             "combines results with multiple pages",
+			startingPageSize: 1,
+			mockResults:      [][]string{{"hello"}},
+			expectedResults:  [][]string{{"hello"}, {"hello"}},
+		},
+		{
+			name:             "pagination fails when client fails to generate a search pattern",
+			mockErr:          command.SearchTooLargeErr,
+			expectedErr:      NoSearchPatternErr,
+			startingPageSize: 2,
+		},
+		{
+			name:             "pagination fails when pageSize is too low",
+			expectedErr:      NoSearchPatternErr,
+			startingPageSize: -1,
+		},
+	}
+
+	for _, tt := range specs {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := simplePaginatedSearch(
+				MockClient{
+					results: tt.mockResults,
+					err:     tt.mockErr,
+				},
+				[]string{"someFlag", "anotherFlag"},
+				tt.startingPageSize,
+				0,
+				[]rune{'"'},
+			)
+			assert.Equal(t, tt.expectedResults, res)
+			assert.Equal(t, tt.expectedErr, err)
+		})
+	}
+}
