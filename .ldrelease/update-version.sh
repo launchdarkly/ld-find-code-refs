0b092836389df495940f4eb9b087205e2aace91a
Dan O'Brien <dobrien@launchdarkly.com>
2021-10-29 08:34:12 -0400

    changes based on release

--- .ldrelease/update-version.sh
+++ .ldrelease/update-version.sh
@@ -7,5 +7,5 @@ mv ${VERSION_GO_TEMP} ${VERSION_GO}
 
 VERSION_ORB=build/package/circleci/orb.yml
 VERSION_ORB_TEMP=${VERSION_ORB}.tmp
-sed -i "s#launchdarkly: launchdarkly/ld-find-code-refs@.*#launchdarkly: launchdarkly/ld-find-code-refs@\"${LD_RELEASE_VERSION}\"#g" ${VERSION_ORB}
-sed -i "s#- image: launchdarkly/ld-find-code-refs:.*#- image: launchdarkly/ld-find-code-refs:\"${LD_RELEASE_VERSION}\"#g" ${VERSION_ORB}
+sed -i "s#launchdarkly: launchdarkly/ld-find-code-refs@.*#launchdarkly: launchdarkly/ld-find-code-refs@${LD_RELEASE_VERSION}#g" ${VERSION_ORB}
+sed -i "s#- image: launchdarkly/ld-find-code-refs:.*#- image: launchdarkly/ld-find-code-refs:${LD_RELEASE_VERSION}#g" ${VERSION_ORB}
