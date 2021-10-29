989978fed5e7a5158adec48142b6cb8ffb55206a
Dan O'Brien <dobrien@launchdarkly.com>
2021-10-29 07:51:07 -0400

    orb path

--- .ldrelease/update-version.sh
+++ .ldrelease/update-version.sh
@@ -5,7 +5,7 @@ VERSION_GO_TEMP=${VERSION_GO}.tmp
 sed "s/const Version =.*/const Version = \"${LD_RELEASE_VERSION}\"/g" ${VERSION_GO} > ${VERSION_GO_TEMP}
 mv ${VERSION_GO_TEMP} ${VERSION_GO}
 
-VERSION_ORB=build/package/circleci/orb.yaml
+VERSION_ORB=build/package/circleci/orb.yml
 VERSION_ORB_TEMP=${VERSION_ORB}.tmp
 sed -i "s#launchdarkly: launchdarkly/ld-find-code-refs@.*#launchdarkly: launchdarkly/ld-find-code-refs@\"${LD_RELEASE_VERSION}\"#g" ${VERSION_ORB}
 sed -i "s#- image: launchdarkly/ld-find-code-refs:.*#- image: launchdarkly/ld-find-code-refs:\"${LD_RELEASE_VERSION}\"#g" ${VERSION_ORB}
