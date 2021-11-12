fdf0ad27a931b300cb0b765a2348412012dffa44
Jaz White <jwhite@launchdarkly.com>
2021-11-12 11:07:52 -0500

    Adds first matcher test

--- search/matcher_test.go
+++ search/matcher_test.go
@@ -12,3 +12,19 @@ func Test_buildDelimiterList(t *testing.T) {
 	want := map[string][]string{"testflag": []string{"\"testflag\"", "\"testflag'", "\"testflag`", "'testflag\"", "'testflag'", "'testflag`", "`testflag\"", "`testflag'", "`testflag`"}}
 	require.Equal(t, want, delimitedFlag)
 }
+
+func Test_MatchElement(t *testing.T) {
+	matcher := Matcher{
+		Delimiters: defaultDelims,
+		Elements: []ElementMatcher{
+			{
+				Elements:       []string{"testflag"},
+				Aliases:        map[string][]string{"testflag": {}},
+				Delimiters:     []string{"\"", "'", "`"},
+				DelimitedFlags: map[string][]string{"testflag": {"\"testflag\"", "\"testflag'", "\"testflag`", "'testflag\"", "'testflag'", "'testflag`", "`testflag\"", "`testflag'", "`testflag`"}},
+			},
+		},
+	}
+
+	require.Equal(t, true, matcher.MatchElement("var featureFlag = 'testflag';", "testflag"))
+}
