d07173946346b9ac6390d99d4c735c351b883937
Andrew S. Brown <andrew@launchdarkly.com>
2021-11-12 07:48:22 -0800

    Add test

--- search/matcher_test.go
+++ search/matcher_test.go
@@ -3,6 +3,7 @@ package search
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -11,4 +12,18 @@ func Test_buildFlagPatterns(t *testing.T) {
 	patterns := buildElementPatterns([]string{testFlagKey}, defaultDelims)
 	want := map[string][]string{"testflag": []string{"\"testflag\"", "\"testflag'", "\"testflag`", "'testflag\"", "'testflag'", "'testflag`", "`testflag\"", "`testflag'", "`testflag`"}}
 	require.Equal(t, want, patterns)
+}
+
+func TestElementMatcher_FindAliases(t *testing.T) {
+	t.Run("overlapping aliases are reported separately", func(t *testing.T) {
+		matcher := NewElementMatcher("project", "", nil, map[string][]string{"flag": {"alias", "alias1"}})
+		assert.ElementsMatch(t, []string{"alias", "alias1"}, matcher.FindAliases("alias1", "flag"))
+	})
+}
+
+func TestElementMatcher_FindMatches(t *testing.T) {
+	t.Run("overlapping flags are reported separately", func(t *testing.T) {
+		matcher := NewElementMatcher("project", "", []string{"flag", "flag1"}, nil)
+		assert.ElementsMatch(t, []string{"flag", "flag1"}, matcher.FindMatches("flag1"))
+	})
 }
