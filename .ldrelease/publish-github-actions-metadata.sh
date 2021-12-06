f2fed128010304a126a61635ed996b9a475b324e
Yusinto Ngadiman <yusinto@gmail.com>
2021-12-06 15:05:09 -0800

    Update publish-github-actions-metadata.sh

--- .ldrelease/publish-github-actions-metadata.sh
+++ .ldrelease/publish-github-actions-metadata.sh
@@ -3,12 +3,21 @@
 # Run this in publish step after all version information have been updated.
 set -ev
 
+# Read from the command line so we can debug this script. Defaults to releaser env variable.
+RELEASE_VERSION=${1:-$LD_RELEASE_VERSION}
+
+rm -rf githubActionsMetadataUpdates
 mkdir -p githubActionsMetadataUpdates
-git clone git@github.com:launchdarkly/find-code-refs.git githubActionsMetadataUpdates
+git clone git@github.com:launchdarkly/find-code-references.git githubActionsMetadataUpdates
 cp build/metadata/github-actions/* githubActionsMetadataUpdates
 cd githubActionsMetadataUpdates
 git add -u
 git commit -m "Release auto update version"
-git push origin master
+
+# TODO: how do we reconcile versions between ld-find-code-refs and github-action? The github action version is v13?
+git tag v$RELEASE_VERSION
+git push origin master --tags
+
+#TODO: create github release
 
 cd .. && rm -rf githubActionsMetadataUpdates
