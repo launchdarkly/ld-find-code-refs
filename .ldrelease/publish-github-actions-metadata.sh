16ecc2e9221e31041c94098a855f10b2e3668930
Yusinto Ngadiman <yusinto@gmail.com>
2021-12-06 14:51:09 -0800

    Update publish-github-actions-metadata.sh

--- .ldrelease/publish-github-actions-metadata.sh
+++ .ldrelease/publish-github-actions-metadata.sh
@@ -4,12 +4,11 @@
 set -ev
 
 mkdir -p githubActionsMetadataUpdates
-#git clone git@github.com:launchdarkly/find-code-refs.git githubActionsMetadataUpdates
-git clone git@github.com:yusinto/find-code-references.git githubActionsMetadataUpdates
+git clone git@github.com:launchdarkly/find-code-refs.git githubActionsMetadataUpdates
 cp build/metadata/github-actions/* githubActionsMetadataUpdates
 cd githubActionsMetadataUpdates
 git add -u
 git commit -m "Release auto update version"
 git push origin master
 
-rm -rf githubActionsMetadataUpdates
+cd .. && rm -rf githubActionsMetadataUpdates
