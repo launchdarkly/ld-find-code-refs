c528266e357f9812d46288bed34b80cdfa3f41e2
Dan O'Brien <dobrien@launchdarkly.com>
2021-12-13 08:20:00 -0500

    revert options change

--- options/options.go
+++ options/options.go
@@ -208,7 +208,7 @@ func (o Options) Validate() error {
 		}
 	}
 
-	o.Dir, err = validation.NormalizeAndValidatePath(o.Dir)
+	_, err = validation.NormalizeAndValidatePath(o.Dir)
 	if err != nil {
 		return fmt.Errorf(`invalid value for "dir": %+v`, err)
 	}
