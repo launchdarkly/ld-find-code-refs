7859817ed452b554b9fd30a3e1f69840e142e699
Yusinto Ngadiman <yusinto@gmail.com>
2022-02-14 14:59:46 -0800

    Update gha and bb version. Removed TODOs.

--- .ldrelease/publish.sh
+++ .ldrelease/publish.sh
@@ -3,7 +3,7 @@
 # the "publish" makefile target pushes the image to Docker
 $(dirname $0)/run-publish-target.sh publish
 
-# TODO: publish to github actions, bitbucket and circleci marketplaces
+# publish to github actions, bitbucket and circleci marketplaces
 $(dirname $0)/publish-bitbucket-metadata.sh
 $(dirname $0)/publish-circleci.sh
 $(dirname $0)/publish-github-actions-metadata.sh
