1820c69c5154ad4b4b759346b87bd5d4fbc50b38
Yusinto Ngadiman <yusinto@gmail.com>
2022-02-16 14:20:52 -0800

    Moved back git config to bitbucket publish script. Uncomment debugging lines.

--- .ldrelease/build.sh
+++ .ldrelease/build.sh
@@ -2,5 +2,4 @@
 
 set -ue
 
-# TODO: Uncomment when done debugging release process
-#make build
+make build
--- .ldrelease/publish.sh
+++ .ldrelease/publish.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # the "publish" makefile target pushes the image to Docker
-# TODO: Uncomment when done debugging release process
-#$(dirname $0)/run-publish-target.sh publish
+$(dirname $0)/run-publish-target.sh publish
 
 # make bitbucket and github known hosts to push successfully
 mkdir –m700 ~/.ssh
@@ -10,10 +9,6 @@ touch ~/.ssh/known_hosts
 chmod 644 ~/.ssh/known_hosts
 ssh-keyscan -t rsa bitbucket.org >> ~/.ssh/known_hosts
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
-
-# set git parameters globally so we can push
-git config --global user.email "yus@launchdarkly.com"
-git config --global user.name "Yus Ngadiman"
 
 # publish to github actions, bitbucket and circleci marketplaces
 $(dirname $0)/publish-bitbucket-metadata.sh
