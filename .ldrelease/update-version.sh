73dd30e481b991e25277f818f56aeaa8b409324f
Jaz White <jwhite@launchdarkly.com>
2022-03-01 13:38:31 -0500

    don't use v in circleci version

--- .ldrelease/update-version.sh
+++ .ldrelease/update-version.sh
@@ -9,7 +9,7 @@ mv ${VERSION_GO_TEMP} ${VERSION_GO}
 
 VERSION_ORB=build/package/circleci/orb.yml
 VERSION_ORB_TEMP=${VERSION_ORB}.tmp
-sed -i "s#launchdarkly: launchdarkly/ld-find-code-refs@.*#launchdarkly: launchdarkly/ld-find-code-refs@v${LD_RELEASE_VERSION}#g" ${VERSION_ORB}
+sed -i "s#launchdarkly: launchdarkly/ld-find-code-refs@.*#launchdarkly: launchdarkly/ld-find-code-refs@${LD_RELEASE_VERSION}#g" ${VERSION_ORB}
 sed -i "s#- image: launchdarkly/ld-find-code-refs:.*#- image: launchdarkly/ld-find-code-refs:v${LD_RELEASE_VERSION}#g" ${VERSION_ORB}
 
 # update github actions and bitbucket metadata as part of automated release
