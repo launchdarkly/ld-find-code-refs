0236cde85aff913d21e2cadc1dea8a0d57dde0e9
Jaz White <jwhite@launchdarkly.com>
2022-03-01 13:23:41 -0500

    version tags should have preceding v

--- .ldrelease/update-version.sh
+++ .ldrelease/update-version.sh
@@ -9,8 +9,8 @@ mv ${VERSION_GO_TEMP} ${VERSION_GO}
 
 VERSION_ORB=build/package/circleci/orb.yml
 VERSION_ORB_TEMP=${VERSION_ORB}.tmp
-sed -i "s#launchdarkly: launchdarkly/ld-find-code-refs@.*#launchdarkly: launchdarkly/ld-find-code-refs@${LD_RELEASE_VERSION}#g" ${VERSION_ORB}
-sed -i "s#- image: launchdarkly/ld-find-code-refs:.*#- image: launchdarkly/ld-find-code-refs:${LD_RELEASE_VERSION}#g" ${VERSION_ORB}
+sed -i "s#launchdarkly: launchdarkly/ld-find-code-refs@.*#launchdarkly: launchdarkly/ld-find-code-refs@v${LD_RELEASE_VERSION}#g" ${VERSION_ORB}
+sed -i "s#- image: launchdarkly/ld-find-code-refs:.*#- image: launchdarkly/ld-find-code-refs:v${LD_RELEASE_VERSION}#g" ${VERSION_ORB}
 
 # update github actions and bitbucket metadata as part of automated release
 $(dirname $0)/update-github-actions-metadata.sh
