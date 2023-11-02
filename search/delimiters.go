5270b69628f668ac72c52b8ba1111acb5daf09f5
Jaz White <jwhite@launchdarkly.com>
2023-11-02 16:17:28 -0400

    add comment

--- search/delimiters.go
+++ search/delimiters.go
@@ -5,6 +5,8 @@ import (
 	"github.com/launchdarkly/ld-find-code-refs/v2/options"
 )
 
+// Get a list of delimiters to use for flag key matching
+// If defaults are disabled, only additional configured delimiters will be used
 func GetDelimiters(opts options.Options) []string {
 	delims := []string{`"`, `'`, "`"}
 	if opts.Delimiters.DisableDefaults {
