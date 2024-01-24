bbbd2b585cfc858b259ecd1d15ab47fff0e3f12b
Jaz White <jwhite@launchdarkly.com>
2024-01-24 10:17:51 -0500

    [sc-112730] fix: out of range error while running extinctions on large diffs (#425)
    Adds a test case to verify the fix. The test data is taken from
    https://github.com/sergi/go-diff/issues/89 data.txt
    
    ---------
    
    Co-authored-by: Lucas Shadler <lucas.shadler@flockfreight.com>

--- go.mod
+++ go.mod
@@ -45,7 +45,7 @@ require (
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
-	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
 	github.com/skeema/knownhosts v1.2.1 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.11.0 // indirect
--- internal/git/git_integration_test.go
+++ internal/git/git_integration_test.go
@@ -1,6 +1,7 @@
 package git
 
 import (
+	"io"
 	"os"
 	"path/filepath"
 	"testing"
@@ -8,6 +9,7 @@ import (
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/launchdarkly/ld-find-code-refs/v2/internal/ld"
@@ -17,10 +19,14 @@ import (
 )
 
 const (
-	repoDir = "testdata/repo"
-	flag1   = "flag1"
-	flag2   = "flag2"
-	flag3   = "flag3"
+	REPO_DIR = "testdata/repo"
+)
+
+var (
+	flag1 = "flag1"
+	flag2 = "flag2"
+	flag3 = "flag3"
+	flag4 = "flag4" // in bigdiff.txt
 )
 
 func TestMain(m *testing.M) {
@@ -29,9 +35,9 @@ func TestMain(m *testing.M) {
 }
 
 func setupRepo(t *testing.T) *git.Repository {
-	os.RemoveAll(repoDir)
-	require.NoError(t, os.MkdirAll(repoDir, 0700))
-	repo, err := git.PlainInit(repoDir, false)
+	os.RemoveAll(REPO_DIR)
+	require.NoError(t, os.MkdirAll(REPO_DIR, 0700))
+	repo, err := git.PlainInit(REPO_DIR, false)
 	require.NoError(t, err)
 	return repo
 }
@@ -41,21 +47,10 @@ func TestFindExtinctions(t *testing.T) {
 	repo := setupRepo(t)
 
 	// Create commit history
-	flagFile, err := os.Create(filepath.Join(repoDir, "flag1.txt"))
-	require.NoError(t, err)
-	_, err = flagFile.WriteString(flag1)
-	require.NoError(t, err)
-	require.NoError(t, flagFile.Close())
-	flagFile, err = os.Create(filepath.Join(repoDir, "flag2.txt"))
-	require.NoError(t, err)
-	_, err = flagFile.WriteString(flag2)
-	require.NoError(t, err)
-	require.NoError(t, flagFile.Close())
-	flagFile, err = os.Create(filepath.Join(repoDir, "flag3.txt"))
-	require.NoError(t, err)
-	_, err = flagFile.WriteString(flag3)
-	require.NoError(t, err)
-	require.NoError(t, flagFile.Close())
+	createRepoFile(t, "flag1.txt", &flag1)
+	createRepoFile(t, "flag2.txt", &flag2)
+	createRepoFile(t, "flag3.txt", &flag3)
+	copyFile(t, "testdata/bigdiff.txt", repoPath("bigdiff.txt"))
 
 	wt, err := repo.Worktree()
 	require.NoError(t, err)
@@ -65,37 +60,42 @@ func TestFindExtinctions(t *testing.T) {
 	wt.Add("flag1.txt")
 	wt.Add("flag2.txt")
 	wt.Add("flag3.txt")
+	wt.Add("bigdiff.txt")
 	_, err = wt.Commit("add flags", &git.CommitOptions{All: true, Committer: &who, Author: &who})
 	require.NoError(t, err)
 
 	// Test with a removed file
-	err = os.Remove(filepath.Join(repoDir, "flag1.txt"))
-	require.NoError(t, err)
+	removeRepoFile(t, "flag1.txt")
 
-	who.When = who.When.Add(time.Minute)
+	who, when2 := incrementCommitTime(who)
 	message2 := "remove flag1"
 	commit2, err := wt.Commit(message2, &git.CommitOptions{All: true, Committer: &who, Author: &who})
 	require.NoError(t, err)
 
 	// Test with an updated (truncated) file
-	flagFile, err = os.Create(filepath.Join(repoDir, "flag2.txt"))
-	require.NoError(t, err)
-	require.NoError(t, flagFile.Close())
+	createRepoFile(t, "flag2.txt", nil)
 
-	who.When = who.When.Add(time.Minute)
+	who, when3 := incrementCommitTime(who)
 	message3 := "remove flag2"
 	commit3, err := wt.Commit("remove flag2", &git.CommitOptions{All: true, Committer: &who, Author: &who})
 	require.NoError(t, err)
 
-	err = os.Remove(filepath.Join(repoDir, "flag3.txt"))
-	require.NoError(t, err)
+	removeRepoFile(t, "flag3.txt")
 
-	who.When = who.When.Add(time.Minute)
+	who, when4 := incrementCommitTime(who)
 	message4 := "remove flag3"
 	commit4, err := wt.Commit("remove flag3", &git.CommitOptions{All: true, Committer: &who, Author: &who})
 	require.NoError(t, err)
 
-	c := Client{workspace: repoDir}
+	// Test big diff
+	removeRepoFile(t, "bigdiff.txt")
+
+	who, when5 := incrementCommitTime(who)
+	message5 := "remove flag4 from bigdiff"
+	commit5, err := wt.Commit(message5, &git.CommitOptions{All: true, Committer: &who, Author: &who})
+	require.NoError(t, err)
+
+	c := Client{workspace: REPO_DIR}
 	projKey := options.Project{
 		Key: "default",
 	}
@@ -103,11 +103,11 @@ func TestFindExtinctions(t *testing.T) {
 		Key: "otherProject",
 	}
 	projects := []options.Project{projKey, addProjKey}
-	missingFlags := [][]string{{flag1, flag2}, {flag3}}
+	missingFlags := [][]string{{flag1, flag2}, {flag3, flag4}}
 	matcher := search.Matcher{
 		Elements: []search.ElementMatcher{
 			search.NewElementMatcher(projKey.Key, ``, ``, []string{flag1, flag2}, nil),
-			search.NewElementMatcher(addProjKey.Key, ``, ``, []string{flag3}, nil),
+			search.NewElementMatcher(addProjKey.Key, ``, ``, []string{flag3, flag4}, nil),
 		},
 	}
 
@@ -122,25 +122,78 @@ func TestFindExtinctions(t *testing.T) {
 		{
 			Revision: commit3.String(),
 			Message:  message3,
-			Time:     who.When.Add(-time.Minute).Unix() * 1000,
+			Time:     when3.UnixMilli(),
 			ProjKey:  projKey.Key,
 			FlagKey:  flag2,
 		},
 		{
 			Revision: commit2.String(),
 			Message:  message2,
-			Time:     who.When.Add(-time.Minute*2).Unix() * 1000,
+			Time:     when2.UnixMilli(),
 			ProjKey:  projKey.Key,
 			FlagKey:  flag1,
 		},
 		{
+			Revision: commit5.String(),
+			Message:  message5,
+			Time:     when5.UnixMilli(),
+			ProjKey:  addProjKey.Key,
+			FlagKey:  flag4,
+		},
+		{
 			Revision: commit4.String(),
 			Message:  message4,
-			Time:     who.When.Unix() * 1000,
+			Time:     when4.UnixMilli(),
 			ProjKey:  addProjKey.Key,
 			FlagKey:  flag3,
 		},
 	}
-	require.Equal(t, expected, extinctions)
 
+	for i, e := range expected {
+		assert.Equalf(t, e, extinctions[i], "exinction at element %d does not match", i)
+	}
+}
+
+// Helper functions
+
+func copyFile(t *testing.T, src, dst string) {
+	sourceFileStat, err := os.Stat(src)
+	require.NoError(t, err)
+	require.Truef(t, sourceFileStat.Mode().IsRegular(), "%s is not a regular file", src)
+
+	source, err := os.Open(src)
+	require.NoError(t, err)
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	require.NoError(t, err)
+
+	defer destination.Close()
+	_, err = io.Copy(destination, source)
+	require.NoError(t, err)
+}
+
+func createRepoFile(t *testing.T, path string, content *string) {
+	flagFile, err := os.Create(repoPath(path))
+	require.NoError(t, err)
+	if content != nil {
+		_, err = flagFile.WriteString(*content)
+		require.NoError(t, err)
+	}
+	require.NoError(t, flagFile.Close())
+}
+
+func removeRepoFile(t *testing.T, path string) {
+	require.NoError(t, os.Remove(filepath.Join(REPO_DIR, path)))
+}
+
+func repoPath(path string) string {
+	return filepath.Join(REPO_DIR, path)
+}
+
+func incrementCommitTime(who object.Signature) (object.Signature, time.Time) {
+	t := who.When.Add(time.Minute)
+	who.When = t
+
+	return who, t
 }
