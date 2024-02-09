431e86c2ebe4a923fcc08033aa9a89d86ceb4bb7
Arek Mano <manoukian.arek@gmail.com>
2024-02-09 13:39:43 -0500

    fix(#429): Multi-project scans now ignore projects without valid flags

--- flags/flags.go
+++ flags/flags.go
@@ -2,7 +2,6 @@ package flags
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/launchdarkly/ld-find-code-refs/v2/internal/helpers"
 	"github.com/launchdarkly/ld-find-code-refs/v2/internal/ld"
@@ -32,18 +31,8 @@ func GetFlagKeys(opts options.Options, repoParams ld.RepoParams) map[string][]st
 		if err != nil {
 			helpers.FatalServiceError(fmt.Errorf("could not retrieve flag keys from LaunchDarkly for project `%s`: %w", proj.Key, err), ignoreServiceErrors)
 		}
-
-		filteredFlags, omittedFlags := filterShortFlagKeys(flags)
-		if len(filteredFlags) == 0 {
-			log.Info.Printf("no flag keys longer than the minimum flag key length (%v) were found for project: %s, exiting early",
-				minFlagKeyLen, proj.Key)
-			os.Exit(0)
-		} else if len(omittedFlags) > 0 {
-			log.Warning.Printf("omitting %d flags with keys less than minimum (%d) for project: %s", len(omittedFlags), minFlagKeyLen, proj.Key)
-		}
-		flagKeys[proj.Key] = filteredFlags
+		addFlagKeys(flagKeys, flags, proj.Key)
 	}
-
 	return flagKeys
 }
 
@@ -60,6 +49,18 @@ func filterShortFlagKeys(flags []string) (filtered []string, omitted []string) {
 		}
 	}
 	return filteredFlags, omittedFlags
+}
+
+func addFlagKeys(flagKeys map[string][]string, flags []string, projKey string) {
+	filteredFlags, omittedFlags := filterShortFlagKeys(flags)
+	if len(filteredFlags) == 0 {
+		log.Warning.Printf("no flag keys longer than the minimum flag key length (%v) were found for project: %s. Skipping project",
+			minFlagKeyLen, projKey)
+		return
+	} else if len(omittedFlags) > 0 {
+		log.Warning.Printf("omitting %d flags with keys less than minimum (%d) for project: %s", len(omittedFlags), minFlagKeyLen, projKey)
+	}
+	flagKeys[projKey] = filteredFlags
 }
 
 func getFlags(ldApi ld.ApiClient, projKey string) ([]string, error) {
