9c648fca265e190c60db20e20b57debc70ae9e14
Daniel OBrien <dobrien@launchdarkly.com>
2024-05-10 17:03:29 -0400

    dagger module

---
+++ ci/dagger/go.mod
@@ -0,0 +1,46 @@
+module dagger/ci
+
+go 1.22.2
+
+require (
+	github.com/99designs/gqlgen v0.17.44
+	github.com/Khan/genqlient v0.7.0
+	github.com/containerd/containerd v1.7.16
+	github.com/vektah/gqlparser/v2 v2.5.11
+	go.opentelemetry.io/otel v1.24.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.24.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.24.0
+	go.opentelemetry.io/otel/sdk v1.24.0
+	go.opentelemetry.io/otel/trace v1.24.0
+	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa
+	golang.org/x/sync v0.6.0
+	google.golang.org/grpc v1.62.1
+)
+
+require (
+	github.com/Microsoft/hcsshim v0.11.4 // indirect
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0-rc2.0.20221005185240-3a7f492d3f1b // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+)
+
+require (
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.0 // indirect
+	github.com/sosodev/duration v1.2.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.24.0 // indirect
+	go.opentelemetry.io/otel/metric v1.24.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.1.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240123012728-ef4313101c80 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240325203815-454cdb8f5daa // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
---
+++ ci/dagger/main.go
@@ -0,0 +1,74 @@
+// A generated module for Ci functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+
+package main
+
+import (
+	"context"
+	"dagger/ci/internal/dagger"
+	"fmt"
+)
+
+type Ci struct{}
+
+// Returns a container that echoes whatever string argument is provided
+func (m *Ci) ContainerEcho(stringArg string) *Container {
+	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
+}
+
+func (m *Ci) BuildAndTest(ctx context.Context, source *Directory) (string, error) {
+	if _, err := m.TestRepo(ctx, source); err != nil {
+		return "", err
+	}
+
+	out, err := m.BuildBinary(ctx, source)
+
+	if err != nil {
+		return out, err
+	}
+
+	return out, nil
+}
+
+// Returns lines that match a pattern in the files of the provided Directory
+func (m *Ci) TestRepo(ctx context.Context, source *Directory) (string, error) {
+	return dag.Container().
+		From("cimg/go:1.21").
+		WithMountedCache("/go/pkg/mod", m.goModCacheVolume()).
+		WithExec([]string{"sudo", "apt-get", "update"}).
+		WithExec([]string{"go", "install", "github.com/jstemmer/go-junit-report@v1.0.0"}).
+		WithExec([]string{"go", "install", "github.com/kyoh86/richgo@v0.3.10"}).
+		WithExec([]string{"sudo", "apt-get", "update"}).
+		WithExec([]string{"sudo", "apt-get", "install", "python3-pip"}).
+		WithExec([]string{"sudo", "pip", "install", "pre-commit"}).
+		WithDirectory("/src", source).WithWorkdir("/src").
+		WithExec([]string{"go", "test", "./..."}).
+		Stdout(ctx)
+}
+
+func (m *Ci) BuildBinary(ctx context.Context, source *dagger.Directory) (string, error) {
+	fmt.Printf("Building binary")
+
+	return dag.Goreleaser(source).
+		withGoCache().
+		Release(ctx)
+}
+
+func (m *Ci) goModCacheVolume() *CacheVolume {
+	return dag.CacheVolume("go-mod")
+}
+
+func (m *Ci) goBuildCacheVolume() *CacheVolume {
+	return dag.CacheVolume("go-build")
+}
