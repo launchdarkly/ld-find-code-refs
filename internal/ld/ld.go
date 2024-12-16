1035caeaa5d06c23b5e0b7055b5722224ae1a8ef
Kevin Kruger <kkruger@launchdarkly.com>
2024-12-16 14:44:16 -0500

    more linting

--- internal/ld/ld.go
+++ internal/ld/ld.go
@@ -77,8 +77,8 @@ func IsTransient(err error) bool {
 // Fallback to default backoff if header can't be parsed
 // https://apidocs.launchdarkly.com/#section/Overview/Rate-limiting
 // Method is curried in order to avoid stubbing the time package and fallback Backoff in unit tests
-func RateLimitBackoff(now func() time.Time, fallbackBackoff h.Backoff) func(minDuration, maxDuration time.Duration, attemptNum int, resp *http.Response) time.Duration {
-	return func(minDuration, maxDuration time.Duration, attemptNum int, resp *http.Response) time.Duration {
+func RateLimitBackoff(now func() time.Time, fallbackBackoff h.Backoff) func(minDuration, max time.Duration, attemptNum int, resp *http.Response) time.Duration {
+	return func(minDuration, max time.Duration, attemptNum int, resp *http.Response) time.Duration {
 		if resp != nil {
 			if resp.StatusCode == http.StatusTooManyRequests {
 				if s, ok := resp.Header["X-Ratelimit-Reset"]; ok {
@@ -96,7 +96,7 @@ func RateLimitBackoff(now func() time.Time, fallbackBackoff h.Backoff) func(minD
 			}
 		}
 
-		return fallbackBackoff(minDuration, maxDuration, attemptNum, resp)
+		return fallbackBackoff(minDuration, max, attemptNum, resp)
 	}
 }
 
@@ -193,7 +193,7 @@ func (c ApiClient) getProjectEnvironment(projKey string) (*ldapi.Environment, er
 }
 
 func (c ApiClient) getFlags(projKey string, params url.Values) ([]ldapi.FeatureFlag, error) {
-	url := c.getPath(fmt.Sprintf("/flags/%s", projKey))
+	url := c.getPath("/flags/" + projKey)
 	req, err := h.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return nil, err
