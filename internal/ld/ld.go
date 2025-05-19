8b8c1faf386600dc5866d123cf9ca8ce39ed9b54
Isabelle Miller <imiller@launchdarkly.com>
2025-05-19 17:31:36 +0200

    linter complainig about use of Sprintf, change to +

--- internal/ld/ld.go
+++ internal/ld/ld.go
@@ -208,7 +208,7 @@ func (c ApiClient) getFlags(projKey string, params url.Values) ([]ldapi.FeatureF
 	}
 
 	var allFlags []ldapi.FeatureFlag
-	nextUrl := c.getPath(fmt.Sprintf("/flags/%s", projKey)) //nolint:perfsprint
+	nextUrl := c.getPath("/flags/%s" + projKey) //nolint:perfsprint
 
 	for nextUrl != "" {
 		req, err := h.NewRequest(http.MethodGet, nextUrl, nil)
