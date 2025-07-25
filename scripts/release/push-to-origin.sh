e7601a182b0fee8303a219fb0f75ec54b586dcb9
Chris Blackburn <cblackburn@launchdarkly.com>
2025-07-25 17:47:32 -0400

    move tag creation above goreleaser

--- scripts/release/push-to-origin.sh
+++ scripts/release/push-to-origin.sh
@@ -9,21 +9,21 @@ tag_exists() (
   git rev-parse "${release_tag}" >/dev/null 2>&1
 )
 
-create_tag() (
+push_to_origin() (
   if tag_exists; then
     echo "Tag $release_tag already exists. Aborting."
     exit 1
   fi
 
-  git tag "${release_tag}"
   git push origin HEAD
   git push origin "${release_tag}"
 )
 
 if [[ "$DRY_RUN" == "true" ]]; then
-  git reset --hard HEAD^
-  echo "Dry run mode: skipping tag and push"
+  git tag -d "$release_tag" # defensive
+  git reset --hard HEAD^    # defensive
+  echo "Dry run mode: skipping push"
 else
-  create_tag
+  push_to_origin
 fi
 
