61893f412c10d27b8fa9dc07b49ef8762204738f
Chris Blackburn <cblackburn@launchdarkly.com>
2025-08-05 11:21:03 -0400

    pass homebrew token through to Makefile

--- scripts/release/stage-artifacts.sh
+++ scripts/release/stage-artifacts.sh
@@ -7,7 +7,7 @@ stage_artifacts() (
 
   echo "$DOCKER_TOKEN" | sudo docker login --username "$DOCKER_USERNAME" --password-stdin
 
-  sudo PATH="$PATH" GITHUB_TOKEN="$GITHUB_TOKEN" make "$target"
+  sudo PATH="$PATH" GITHUB_TOKEN="$GITHUB_TOKEN" HOMEBREW_GH_TOKEN="$HOMEBREW_GH_TOKEN" make "$target"
 
   mkdir -p "$ARTIFACT_DIRECTORY"
   cp ./dist/*.deb ./dist/*.rpm ./dist/*.tar.gz ./dist/*.txt "$ARTIFACT_DIRECTORY"
