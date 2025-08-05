af63567b12824c30af6525df1c634ae34fdfe240
Chris Blackburn <cblackburn@launchdarkly.com>
2025-08-05 16:05:09 -0400

    write deploy key to file instead of adding contents directly

--- scripts/release/stage-artifacts.sh
+++ scripts/release/stage-artifacts.sh
@@ -7,7 +7,15 @@ stage_artifacts() (
 
   echo "$DOCKER_TOKEN" | sudo docker login --username "$DOCKER_USERNAME" --password-stdin
 
-  sudo PATH="$PATH" GITHUB_TOKEN="$GITHUB_TOKEN" HOMEBREW_GH_TOKEN="$HOMEBREW_GH_TOKEN" make "$target"
+  # write homebrew key to temporary file for Goreleaser
+  if [[ -n "${HOMEBREW_GH_TOKEN:-}" ]]; then
+    HOMEBREW_KEY_PATH="/tmp/homebrew-tap-deploy-key"
+    echo "$HOMEBREW_GH_TOKEN" > "$HOMEBREW_KEY_PATH"
+    chmod 600 "$HOMEBREW_KEY_PATH"
+    export HOMEBREW_KEY_PATH
+  fi
+
+  sudo PATH="$PATH" GITHUB_TOKEN="$GITHUB_TOKEN" HOMEBREW_KEY_PATH="$HOMEBREW_KEY_PATH" make "$target"
 
   mkdir -p "$ARTIFACT_DIRECTORY"
   cp ./dist/*.deb ./dist/*.rpm ./dist/*.tar.gz ./dist/*.txt "$ARTIFACT_DIRECTORY"
