03fc0ff9f6f0585c99b848c0a43a20612f4f354c
Chris Blackburn <cblackburn@launchdarkly.com>
2025-08-12 11:32:55 -0400

    use https to ensure use of elevated token

--- scripts/release/push-to-origin.sh
+++ scripts/release/push-to-origin.sh
@@ -5,14 +5,16 @@ set -euo pipefail
 release_tag="v$LD_RELEASE_VERSION"
 
 tag_exists() (
-  git ls-remote --tags git@github.com:launchdarkly/ld-find-code-refs.git "refs/tags/$release_tag" | grep -q "$release_tag"
+  git ls-remote --tags https://github.com/launchdarkly/ld-find-code-refs.git "refs/tags/$release_tag" | grep -q "$release_tag"
 )
 
 push_to_origin() (
   if tag_exists; then
-    echo "Tag $release_tag already exists. Aborting."
+    echo "Tag $release_tag already exists. Skipping push."
     return 0
   fi
+
+  git remote set-url origin https://github.com/launchdarkly/ld-find-code-refs.git
 
   git push origin HEAD
   git push origin "$release_tag"
